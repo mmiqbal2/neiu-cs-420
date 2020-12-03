a2575be375e1ba1de65ed46fcb8288b2cc26df1a
mmiqbal2 <mmiqbal2@neiu.edu>
2020-12-03 16:18:48 -0600

    Final Project

---
+++ FinalProject/src/main/java/countriesfx/models/IRetriveData.java
@@ -0,0 +1,12 @@
+package countriesfx.models;
+
+import getapidata.WriteData;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+public interface IRetriveData {
+
+    void readApi(WriteData f, HttpURLConnection connection) throws IOException;
+    void connection (String urlString) throws IOException ;
+}
